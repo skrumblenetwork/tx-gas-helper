e57e10235107d1e463f024919fb79cd30c2aa945
Girish <girish979@users.noreply.github.com>
2020-07-22 11:05:19 -0400

    Create Subscriber

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+'use strict';
+
+const Web3Utils = require("./Utils/Web3Utils.js");
+const redis = require('redis');
+
+const subscriber = redis.createClient(config.redisPubsub);
+
+subscriber.on('message', (channel, message) => {
+  const data = JSON.parse(message);
+  Web3Utils.transferGas(data.userId, data.userWallet, data.destinationWallet, data.amount, data.contract);
+});
+
+subscriber.subscribe('claimGas');
